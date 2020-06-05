5c6ff3e579826c391c9051e0c77ab50409851891
IgorFilipeSilvaMachado <56729271+IgorFilipeSilvaMachado@users.noreply.github.com>
2020-06-05 18:38:59 +0100

    Add files via upload

---
+++ JavaApresentação.java
@@ -0,0 +1,26 @@
+ 
+
+Int = 28; //quando se quer declarar uma variavel como inteira
+
+Float = 15.4; //quando se quer declarar um numero com virgulas
+
+Char = "m" //quando se quer apresentar um so caracter 
+
+String = "Declarar Variaveis" //quando se quer apresentar textos ou frases
+
+Numero = false; // quando é falso
+
+Numero = true; // quando  é verdadeiro
+
+
+/*
+*/ quando se quer comentar alguma coisa ou //
+
+
+//funçao if
+if (i>0, i=6, i++){
+	Printf("foi a cozinha");
+}
+else{
+	Printf("foi a garagem");
+}
